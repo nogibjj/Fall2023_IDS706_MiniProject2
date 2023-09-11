baf36f66102239792de2e440dd87bfbc35f1615b
JiayiZhou36 <143651921+JiayiZhou36@users.noreply.github.com>
2023-09-11 01:52:57 +0000

    "modified"

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 """Main function goes here"""
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 def describe_file(input1):
     """define a function read a file"""
     df1 = pd.read_csv(input1)
-    return df1.describe()
+    summary_statistics= df1.describe()
+    summary_html = summary_statistics.to_html("summary_statistics.html")
+    return summary_statistics, summary_html
+
+def plot_file(input1):
+    """define a function plot relationship in a dataframe"""
+    df1 = pd.read_csv(input1)
+    df1.plot(x='CAPFAC', y='PLNGENAN')
+    # Save the plot as a figure
+    plt.savefig('plant_capacity_factor.png')
--- test_main.py
+++ test_main.py
@@ -2,14 +2,17 @@
 Test Cases
 """
 from main import describe_file
+from main import plot_file
 
 
 def test_read():
     """test function"""
-    df1 = describe_file("https://www.dropbox.com/s/x2awp0e9znsyub7/egrid2016.csv?dl=1")
+    #read a csv file by url
+    df1 = describe_file("https://www.dropbox.com/s/x2awp0e9znsyub7/egrid2016.csv?dl=1")[0]
     assert df1["SEQPLT16"]["mean"] == 4855.0
     assert df1["SEQPLT16"]["count"] == 9709.0
     assert df1["CAPFAC"]["count"] == 8038.0
+    plot_file("https://www.dropbox.com/s/x2awp0e9znsyub7/egrid2016.csv?dl=1")
 
 if __name__ == "__main__":
     test_read()
